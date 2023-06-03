3313457b4387ce18aea7a038ce746b53eb221958
Pål Hoven Bentsen <pabentse@ifi.uio.no>
2023-06-03 16:00:34 +0200

    asdf

--- perc-calc/src/App.js
+++ perc-calc/src/App.js
@@ -15,17 +15,20 @@ function App() {
   const [input6, setInput6] = useState('');
   const [output3, setOutput3] = useState('');
 
-  const handleCalculate1 = () => {
+  const handleCalculate1 = (e) => {
+    e.preventDefault();
     const result = (input1 / 100) * input2;
     setOutput1(result.toFixed(2));
   }
 
-  const handleCalculate2 = () => {
+  const handleCalculate2 = (e) => {
+    e.preventDefault();
     const result = (input3 / input4) * 100;
     setOutput2(result.toFixed(2));
   }
   
-  const handleCalculate3 = () => {
+  const handleCalculate3 = (e) => {
+    e.preventDefault();
     const result = ((input6 - input5) / input5) * 100;
     setOutput3(result.toFixed(2));
   }
@@ -34,26 +37,26 @@ function App() {
   // JSX code in App.js
 return (
   <main className="calculator">
-    <div className="form">
+      <form className="form" onSubmit={handleCalculate1}>
       What is 
       <input className="calculator-input" value={input1} onChange={e => setInput1(e.target.value)} /> % of 
       <input className="calculator-input" value={input2} onChange={e => setInput2(e.target.value)} />?
       <button className="calculator-button" data-testid="calculate1-button" onClick={handleCalculate1}>Calculate</button>
       <div className="results">{output1}</div>
-    </div>
-    <div className="form">
+    </form>
+    <form className="form" onSubmit={handleCalculate2}>
       <input className="calculator-input" value={input3} onChange={e => setInput3(e.target.value)} /> is what percentage of 
       <input className="calculator-input" value={input4} onChange={e => setInput4(e.target.value)} />?
       <button className="calculator-button" data-testid="calculate2-button" onClick={handleCalculate2}>Calculate</button>
       <div className="results">{output2}</div>
-    </div>
-    <div className="form">
+    </form>
+    <form className="form" onSubmit={handleCalculate3}>
       What is the percentage increase/decrease from 
       <input className="calculator-input" value={input5} onChange={e => setInput5(e.target.value)} /> to 
       <input className="calculator-input" value={input6} onChange={e => setInput6(e.target.value)} />?
       <button className="calculator-button" data-testid="calculate3-button" onClick={handleCalculate3}>Calculate</button>
       <div className="results">{output3}</div>
-    </div>
+    </form>
   </main>
 );
 }
