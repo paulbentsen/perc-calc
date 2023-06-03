97c2a0172b68697a1305cb192ffa5fa05894580e
Pål Hoven Bentsen <pabentse@ifi.uio.no>
2023-06-03 16:46:16 +0200

    alskdfhasdlf

--- perc-calc/src/App.js
+++ perc-calc/src/App.js
@@ -32,29 +32,28 @@ function App() {
     const result = ((input6 - input5) / input5) * 100;
     setOutput3(result.toFixed(2));
   }
-  
 
   // JSX code in App.js
 return (
   <main className="calculator">
       <form className="form" onSubmit={handleCalculate1}>
-      What is 
-      <input className="calculator-input" value={input1} onChange={e => setInput1(e.target.value)} /> % of 
-      <input className="calculator-input" value={input2} onChange={e => setInput2(e.target.value)} />?
-      <button className="calculator-button" data-testid="calculate1-button" onClick={handleCalculate1}>Calculate</button>
-      <div className="results">{output1}</div>
-    </form>
+        What is 
+        <input className="calculator-input" value={input1} onChange={e => setInput1(e.target.value)} /> % of 
+        <input className="calculator-input" value={input2} onChange={e => setInput2(e.target.value)} />?
+        <button className="calculator-button" data-testid="calculate1-button" type="submit">Calculate</button>
+        <div className="results">{output1}</div>
+      </form>
     <form className="form" onSubmit={handleCalculate2}>
       <input className="calculator-input" value={input3} onChange={e => setInput3(e.target.value)} /> is what percentage of 
       <input className="calculator-input" value={input4} onChange={e => setInput4(e.target.value)} />?
-      <button className="calculator-button" data-testid="calculate2-button" onClick={handleCalculate2}>Calculate</button>
+      <button className="calculator-button" data-testid="calculate2-button" type="submit">Calculate</button>
       <div className="results">{output2}</div>
     </form>
     <form className="form" onSubmit={handleCalculate3}>
       What is the percentage increase/decrease from 
       <input className="calculator-input" value={input5} onChange={e => setInput5(e.target.value)} /> to 
       <input className="calculator-input" value={input6} onChange={e => setInput6(e.target.value)} />?
-      <button className="calculator-button" data-testid="calculate3-button" onClick={handleCalculate3}>Calculate</button>
+      <button className="calculator-button" data-testid="calculate3-button" type="submit">Calculate</button>
       <div className="results">{output3}</div>
     </form>
   </main>
